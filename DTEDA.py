cafedee6b22ff2268630305f74a234ed16df7687
jind2021 <31631979+0oTinaKimo0@users.noreply.github.com>
2020-09-25 17:35:21 -0400

    Initial working version
    Accuracy around 74%

---
+++ DTEDA.py
@@ -0,0 +1,21 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import pandas as pd
+import matplotlib.pyplot as plt
+from mlxtend.plotting import scatterplotmatrix
+import numpy as np
+from mlxtend.plotting import heatmap
+
+# Load dataset
+diabetes = pd.read_csv('diabetes.csv', header=0)
+diabetes.columns = ['PREG', 'GLU', 'BP', 'SKIN', 'INSU', 'BMI', 'DPF', 'AGE', 'OUT']
+features = ['PREG', 'GLU', 'BP', 'SKIN', 'INSU', 'BMI', 'DPF', 'AGE']
+X = diabetes[features].values
+y = diabetes['OUT'].T
+
+# EDA
+cm = np.corrcoef(diabetes[diabetes.columns].values.T)
+hm = heatmap(cm, row_names=diabetes.columns, column_names=diabetes.columns)
+
+scatterplotmatrix(diabetes[features].values, figsize=(10, 8), names=features, alpha=0.4)
+plt.show()
